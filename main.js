92fb4f279f2ae50885b77371d25202a7f380cae2
Declan Byrd <declan.byrd@gmail.com>
2020-06-19 21:19:33 +0100

    fix: touch events not firing on mobile

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById('board');
 const context = canvas.getContext('2d');
 
+const ongoingTouches = [];
 const lineColour = 'black';
 let painting = false;
 
@@ -72,13 +73,91 @@ const handlePointerUp = (event) => {
 	painting = false;
 };
 
+const handleTouchStart = (event) => {
+	event.preventDefault();
+	const touches = event.changedTouches;
+
+	for (let index = 0; index < touches.length; index++) {
+		ongoingTouches.push(copyTouch(touches[index]));
+
+		context.beginPath();
+		context.lineTo(touches[index].pageX, touches[index].pageY);
+		context.lineWidth = 4;
+		context.strokeStyle = lineColour;
+		context.stroke();
+	}
+};
+
+const handleTouchMove = (event) => {
+	event.preventDefault();
+	const touches = event.changedTouches;
+
+	for (let index = 0; index < touches.length; index++) {
+		const identifier = findTouch(touches[index]);
+
+		if (identifier >= 0) {
+			context.beginPath();
+			context.moveTo(
+				ongoingTouches[identifier].pageX,
+				ongoingTouches[identifier].pageY
+			);
+			context.lineTo(touches[index].pageX, touches[index].pageY);
+			context.lineWidth = 4;
+			context.strokeStyle = lineColour;
+			context.stroke();
+
+			ongoingTouches.splice(identifier, 1, copyTouch(touches[index]));
+		}
+	}
+};
+
+const handleTouchEnd = (event) => {
+	event.preventDefault();
+	const touches = event.changedTouches;
+
+	for (let index = 0; index < touches.length; index++) {
+		const identifier = findTouch(touches[index]);
+		if (identifier >= 0) {
+			context.lineWidth = 4;
+			context.fillStyle = lineColour;
+			context.beginPath();
+			context.moveTo(
+				ongoingTouches[identifier].pageX,
+				ongoingTouches[identifier].pageY
+			);
+			context.lineTo(touches[index].pageX, touches[index].pageY);
+			context.arc(
+				touches[index].pageX,
+				touches[index].pageY,
+				2,
+				0,
+				2 * Math.PI
+			);
+			context.fillStyle = lineColour;
+			context.fill();
+			ongoingTouches.splice(identifier, 1, touches[index]);
+		}
+	}
+};
+
+const addPointerListeners = () => {
+	canvas.addEventListener('pointerdown', handlePointerDown);
+	canvas.addEventListener('pointermove', handlePointerMove);
+	canvas.addEventListener('pointerup', handlePointerUp);
+};
+
+const addTouchListeners = () => {
+	canvas.addEventListener('touchstart', handleTouchStart);
+	canvas.addEventListener('touchmove', handleTouchMove);
+	canvas.addEventListener('touchend', handleTouchEnd);
+};
+
 // Initialise the canvas and add event listeners.
 const init = () => {
 	const pixelRatio = resize(canvas);
 	context.scale(pixelRatio, pixelRatio);
-	canvas.addEventListener('pointerdown', handlePointerDown);
-	canvas.addEventListener('pointermove', handlePointerMove);
-	canvas.addEventListener('pointerup', handlePointerUp);
+	addPointerListeners();
+	addTouchListeners();
 };
 
 window.addEventListener('load', init);
