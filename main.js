09bb5f4ae11cfc1c89a7082b886556ff68e63108
Declan Byrd <declan.byrd@gmail.com>
2020-06-19 21:07:25 +0100

    feat: handle touch and mouse events

--- main.js
+++ main.js
@@ -2,11 +2,7 @@ const canvas = document.getElementById('board');
 const context = canvas.getContext('2d');
 
 const lineColour = 'black';
-const ongoingTouches = [];
-
-/**
- * Utility Functions
- */
+let painting = false;
 
 // Only store the touch data that is needed.
 const copyTouch = ({ identifier, pageX, pageY }) => {
@@ -34,86 +30,55 @@ const resize = (canvas) => {
 	canvas.style.width = `${width}px`;
 	canvas.style.height = `${height}px`;
 
-	return { width, height, pixelRatio };
+	return pixelRatio;
 };
 
 /**
- * Touch Events
+ * 	Handles pointer down event
+ * 	Will be fired when mousedown and touchstart occur.
  */
-
-const handleTouchStart = (event) => {
+const handlePointerDown = (event) => {
 	event.preventDefault();
-	const touches = event.changedTouches;
+	painting = true;
 
-	for (let index = 0; index < touches.length; index++) {
-		ongoingTouches.push(copyTouch(touches[index]));
+	context.beginPath();
+	context.moveTo(event.pageX, event.pageY);
+};
 
-		context.beginPath();
-		context.arc(touches[index].pageX, touches[index].pageY, 2, 0, 2 * Math.PI);
-		context.fillStyle = lineColour;
-		context.fill();
+/**
+ * 	Handles pointer move event
+ * 	Will be fired when mousemove and touchmove occur.
+ */
+const handlePointerMove = (event) => {
+	if (painting) {
+		event.preventDefault();
+
+		context.lineTo(event.pageX, event.pageY);
+		context.lineWidth = 4;
+		context.strokeStyle = lineColour;
+		context.stroke();
 	}
 };
 
-const handleTouchMove = (event) => {
+/**
+ * 	Handles pointer up event
+ * 	Will be fired when mouseup and touchend occur.
+ */
+const handlePointerUp = (event) => {
 	event.preventDefault();
-	const touches = event.changedTouches;
-
-	for (let index = 0; index < touches.length; index++) {
-		const identifier = findTouch(touches[index]);
-
-		if (identifier >= 0) {
-			context.beginPath();
-			context.moveTo(
-				ongoingTouches[identifier].pageX,
-				ongoingTouches[identifier].pageY
-			);
-			context.lineTo(touches[index].pageX, touches[index].pageY);
-			context.lineWidth = 4;
-			context.strokeStyle = lineColour;
-			context.stroke();
-
-			ongoingTouches.splice(identifier, 1, copyTouch(touches[index]));
-		}
-	}
-};
-
-const handleTouchEnd = (event) => {
-	event.preventDefault();
-	const touches = event.changedTouches;
-
-	for (let index = 0; index < touches.length; index++) {
-		const identifier = findTouch(touches[index]);
-		if (identifier >= 0) {
-			context.lineWidth = 4;
-			context.fillStyle = lineColour;
-			context.beginPath();
-			context.moveTo(
-				ongoingTouches[identifier].pageX,
-				ongoingTouches[identifier].pageY
-			);
-			context.lineTo(touches[index].pageX, touches[index].pageY);
-			context.arc(
-				touches[index].pageX,
-				touches[index].pageY,
-				2,
-				0,
-				2 * Math.PI
-			);
-			context.fillStyle = lineColour;
-			context.fill();
-			ongoingTouches.splice(identifier, 1, touches[index]);
-		}
-	}
+	context.lineWidth = 4;
+	context.fillStyle = lineColour;
+	context.closePath();
+	painting = false;
 };
 
 // Initialise the canvas and add event listeners.
 const init = () => {
-	const { width, height, pixelRatio } = resize(canvas);
+	const pixelRatio = resize(canvas);
 	context.scale(pixelRatio, pixelRatio);
-	canvas.addEventListener('touchstart', handleTouchStart);
-	canvas.addEventListener('touchmove', handleTouchMove);
-	canvas.addEventListener('touchend', handleTouchEnd);
+	canvas.addEventListener('pointerdown', handlePointerDown);
+	canvas.addEventListener('pointermove', handlePointerMove);
+	canvas.addEventListener('pointerup', handlePointerUp);
 };
 
 window.addEventListener('load', init);
